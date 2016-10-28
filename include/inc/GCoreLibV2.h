9aee9a222bdc3ec789f0e85fa2d129b49e29d4fd
Tom Jaejoon Lee <serialkk@naver.com>
2016-10-29 00:25:10 +0900

    update .gitignore and update debug text output in CoreLibV2

--- include/inc/GCoreLibV2.h
+++ include/inc/GCoreLibV2.h
@@ -8,6 +8,10 @@
 class GCoreLibV2 : public GWindow
 {
 public:
+	TCHAR m_pFPSBuffer[256];		// FPS 출력
+	TCHAR m_pHWInfoBuffer[256];		// HW 정보 출력
+	TCHAR m_pScreenInfoBuffer[256];	// Screen Resolution 정보 출력
+
 	GTimer					m_Timer;
 	GDirectWrite			m_Font;
 	UINT					m_iPrimitiveType;
