d08f450868576608a54227a325672c3f79b0b716
Tom Jaejoon Lee <serialkk@naver.com>
2016-11-07 17:56:57 +0900

    [eng][main] apply state pattern for seperating game seqence in ENP Main Game Proj

--- ENPGame/ENPGame/GProjMain.h
+++ ENPGame/ENPGame/GProjMain.h
@@ -1,67 +1,22 @@
 #pragma once
 
-
+enum G_SEQ {
+	G_SEQ_END = 0,
+	G_SEQ_OVER = 1,
+	G_SEQ_HOWTO =2,
+	G_SEQ_INTRO =3,
+	G_SEQ_MENU = 4,
+	G_SEQ_SINGLE = 5,
+	G_SEQ_SURVIVAL = 6,
+	G_SEQ_CNT
+};
 
 class GProjMain : public GCoreLibV2
 {
-#ifdef G_MACRO_EFFECT_ADD
 public:
-	GPlaneShape						m_BigPlane;
-	shared_ptr<GSprite>				m_pSprite;
-	ComPtr<ID3D11PixelShader>		m_pPS;
-	D3DXVECTOR4						m_vMaterial;
-	D3DXMATRIX						m_matPlaneWorld;
-	D3DXMATRIX						m_mPlanWorld;
-	//D3DXMATRIX				m_matWorld;
-	shared_ptr<GPlaneShape>			m_pPlane;
-	shared_ptr<GPlaneShape>			m_pScreen;
-//public:
-//	bool check; // play button check
-//	ComPtr<ID3D11BlendState>		m_pAlphaBlendFactor;
-//	ComPtr<ID3D11BlendState>		m_pAlphaBlend;
-#endif
-#ifdef G_MACRO_MAP_ADD
-	//--------------------------------------------------------------------------------------
-	// 오브젝트
-	//--------------------------------------------------------------------------------------
-	//GMapObject*			m_pMapObj;
-	//GBoxShape*			m_pBoxs;
-	//--------------------------------------------------------------------------------------
-	// 쿼드트리
-	//--------------------------------------------------------------------------------------
-	GQuadTreeIndex	m_QuadTree;
-	//--------------------------------------------------------------------------------------
-	// 노이즈 맵
-	//--------------------------------------------------------------------------------------
-	GNoiseMap		m_NoiseMap;
-	//--------------------------------------------------------------------------------------
-	// 미니맵
-	//--------------------------------------------------------------------------------------
-	//GMiniMap		m_MiniMap;
-	//--------------------------------------------------------------------------------------
-	// 디버깅 용도
-	//--------------------------------------------------------------------------------------
-	GLineShape		m_DrawLine;
-	bool			m_bDebugRender;
-	int				m_iDrawDepth;
-	ComPtr<ID3D11PixelShader>   m_pPixelShader;// 프로스텀 전용 픽쉘쉐이더
+	GSeq * m_pGameSeq[G_SEQ_CNT];
+	GSeq * m_pCurrentSeq;
 
-
-	void		DrawSelectTreeLevel(D3DXMATRIX* pView, D3DXMATRIX* pProj);
-	bool		DrawQuadLine(GNode* pNode);
-#endif
-	shared_ptr<GCamera > m_pMainCamera;
-	D3DXMATRIX  m_matWorld;
-#ifdef G_MACRO_CHAR_ADD	
-public:
-	//--------------------------------------------------------------------------------------
-	// 파일 선택하여 로드( 단축기 : O )
-	//--------------------------------------------------------------------------------------
-	vector<shared_ptr<GHeroObj>>	m_HeroObj;
-	T_STR_VECTOR m_LoadFiles;
-	bool		Load();
-	bool		LoadFileDlg(TCHAR* szExt, TCHAR* szTitle);
-#endif
 public:	
 	bool		Init();
 	bool		Frame();
@@ -78,3 +33,5 @@ public:
 	GProjMain(void);
 	virtual ~GProjMain(void);
 };
+
+extern GProjMain*	g_pMain; //외부 연결 범위 선언
