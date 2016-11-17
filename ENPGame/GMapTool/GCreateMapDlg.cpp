5d81d92bf15cf405fd5b4073fc13e0017de332c3
willlead <willlead@naver.com>
2016-11-17 20:23:47 +0900

    [map_tool]  add save & static load image

--- ENPGame/GMapTool/GCreateMapDlg.cpp
+++ ENPGame/GMapTool/GCreateMapDlg.cpp
@@ -37,6 +37,7 @@ void GCreateMapDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(GCreateMapDlg, CDialogEx)
 	
 	ON_BN_CLICKED(IDC_BUTTON1, &GCreateMapDlg::OnBnClickedButton1)
+	ON_EN_CHANGE(IDC_EDIT7, &GCreateMapDlg::OnEnChangeEdit7)
 END_MESSAGE_MAP()
 
 
@@ -89,8 +90,21 @@ void GCreateMapDlg::OnBnClickedButton1()
 	if (dlg.DoModal() == IDOK)
 	{
 		m_strCharName = dlg.GetFileName();
+		//m_strCharName = dlg.GetPathName();
+
 		UpdateData(FALSE);
 		//MessageBox(dlg.GetPathName());
 	}
 
+}
+
+
+void GCreateMapDlg::OnEnChangeEdit7()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CDialogEx::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
 }
