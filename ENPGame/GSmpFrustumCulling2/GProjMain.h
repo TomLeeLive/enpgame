50a20018446b98ec6d1781b7ca69f171945a6ccb
willlead <willlead@naver.com>
2016-11-27 18:20:53 +0900

    [map] culling 2

--- ENPGame/GSmpFrustumCulling2/GProjMain.h
+++ ENPGame/GSmpFrustumCulling2/GProjMain.h
@@ -1,27 +1,6 @@
 #pragma once
 
-enum G_OBJECT {
-	G_OBJ_LAB = 0,
-	//G_OBJ_DROPSHIP,
-	//G_OBJ_CAR1,
-	//G_OBJ_CAR2,
-	G_OBJ_CNT
-};
-
-//오브젝트 위치
-#define G_OBJ_LOC_LAB _T("data/object/lab/lab.GBS")
-#define G_OBJ_LOC_DROPSHIP_LAND _T("data/object/dropship/dropship_land.GBS")
-#define G_OBJ_LOC_CAR _T("data/object/car/car.GBS")
-
-//오브젝트 매니저 등록 이름.
-#define G_OBJ_NAME_LAB L"lab.GBS"
-#define G_OBJ_NAME_DROPSHIP_LAND L"dropship_land.GBS"
-#define G_OBJ_NAME_CAR L"car.GBS"
-
-//셰이더 파일 위치
-#define G_SHA_PLANE L"data/shader/plane.hlsl"
-#define G_SHA_LINE L"data/shader/line.hlsl"
-#define G_SHA_BOX L"data/shader/box.hlsl"
+#define NUM_OBJECTS 100
 
 struct SimpleVertex
 {
@@ -36,14 +15,20 @@ public:
 	GMap			m_CustomMap;
 	GLineShape				m_LineDraw;
 
-	//obj
-	GModel*		m_Obj[G_OBJ_CNT];
-	D3DXMATRIX  m_matObjScl[G_OBJ_CNT], m_matObjRot[G_OBJ_CNT], m_matObjTrans[G_OBJ_CNT];
-	D3DXMATRIX	m_matObjWld[G_OBJ_CNT];
 
 	//frustum
-	D3DXVECTOR3	m_vFrustum[8];	/// 프러스텀을 구성할 정점 8개
-	G_PLANE		m_Plane[6];
+	//D3DXVECTOR3	m_vFrustum[8];	/// 프러스텀을 구성할 정점 8개
+	//G_PLANE		m_Plane[6];
+
+	ComPtr<ID3D11PixelShader>   m_pPixelShader;// 프로스텀 전용 픽쉘쉐이더
+	G_BOX m_gBoxBase;
+
+	D3DXMATRIX					m_matBoxWorld[NUM_OBJECTS];
+	D3DXVECTOR3					m_vBoxPosition[NUM_OBJECTS];
+	D3DXVECTOR4					m_vBoxColor[NUM_OBJECTS];
+
+	G_BOX						m_GBox[NUM_OBJECTS];
+	GShape*						m_pBoxShape;
 public:	
 	bool		Init();
 	bool		Frame();
