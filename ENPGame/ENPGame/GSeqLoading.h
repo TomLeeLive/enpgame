e25dc464c1fcae8ff9ce6af116bbbcfde372cc10
Tom Jaejoon Lee <serialkk@naver.com>
2016-12-19 15:32:26 +0900

    [main] ENPGame proj update

--- ENPGame/ENPGame/GSeqLoading.h
+++ ENPGame/ENPGame/GSeqLoading.h
@@ -1,15 +1,16 @@
 #pragma once
-class GSeqMenu : public GSeq
+class GSeqLoading : public GSeq
 {
 public:
-	static GSeq* CreateInstance() {
-		if (pInstance_ == 0) pInstance_ = new GSeqMenu;
+	static GSeq* CreateInstance() { 
+		if (pInstance_ == 0) pInstance_ = new GSeqLoading;
 		return pInstance_;
 	}
 	bool Init();
-	bool Frame();
-	bool Render();
-	bool Release() { return true; };
+	 bool Frame() ;
+	 bool RenderManually(void* arg1 = NULL, void* arg2 = NULL);
+	 bool Render();
+	 bool Release() { return true; };
 
 	//----------------------------------------------------
 	// 변경된 클라이언트 영역를 재설정을 위한 소멸 및 생성
@@ -18,11 +19,12 @@ public:
 	HRESULT		DeleteResource() { HRESULT hr; return S_OK; };
 
 	int			WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) { return -1; };
-
 public:
-	virtual ~GSeqMenu() {};
+	virtual ~GSeqLoading() {};
 protected:
-	GSeqMenu() {};
+	GSeqLoading() { pInstance_ = 0; };
 private:
-	static GSeqMenu *pInstance_;
+	static GSeqLoading *pInstance_;
 };
+
+
