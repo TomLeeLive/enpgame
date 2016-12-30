91e3bc9f59e900d3f01662073161a3d81ba14f65
Tom Jaejoon Lee <serialkk@naver.com>
2016-12-31 00:34:51 +0900

    [map] change gbs loading dlg code

--- ENPGame/GMapTool/GObjForm.cpp
+++ ENPGame/GMapTool/GObjForm.cpp
@@ -97,7 +97,9 @@ void GObjForm::OnBnClickedButtonload()
 	
 	CString strObjFile;
 
-	CFileDialog dlg(TRUE);
+	CFileDialog dlg(TRUE, NULL, NULL, OFN_HIDEREADONLY, _T("GBS File(*.gbs)|*.gbs|All Files(*.*)|*.*||"));
+
+	//CFileDialog dlg(TRUE);
 	if (dlg.DoModal() == IDOK)
 	{
 		//dlg.GetPathName();
